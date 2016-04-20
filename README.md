90aa1ec9934db6f75a87d453afffba991a826294
Strokoff <strokoffdn@gmail.com>
2016-04-21 00:08:22 +0300

    add command

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # halop
 Nodejs telegram bot
 
-Simple javascript for fun)
+Simple javascript telegram bot just for fun)
+
 Excuses from https://github.com/littleguga/excuse-generator
+Cats images from http://thecatapi.com/
+Jokes from http://shortiki.com
+
+#Доступные команды
+-!котики - картинка с котиками
+-!психотерапия - комплимент девушке
+-!отмазка {Name} - Генерирует отмазку
+-!шутка - Получает шутку с шортиков
--- halop.js
+++ halop.js
@@ -4,22 +4,37 @@ var request = require("request");
 var Generator = require('./excusesgenerator');
 var Psihoterapia = require('./psihoterapia');
 var Like = require('./likes');
-
 var bot = new TelegramBot(token, {polling: true});
+
 
 //Читаем сообщения)
 bot.on('message', function (msg) {
+	if(msg) {
+		console.log(msg.text, msg.from.id);
     //Получаем шортики
 	if(msg.text.indexOf('!шутка') != -1) {
 		request({
 		url: "http://shortiki.com/export/api.php?format=json&type=random&amount=1",
 		json: true
 	}, function (error, response, body) {
-
-	if (!error && response.statusCode === 200) {
-	  	bot.sendMessage(msg.chat.id, ''+ body[0].content);
+    	if (!error && response.statusCode === 200) {
+    	  	bot.sendMessage(msg.chat.id, ''+ body[0].content);
 		}
 	});
+	}
+
+    //Котаны
+	if(msg.text.indexOf('!котики') != -1) {
+		request({
+		url: "http://thecatapi.com/api/images/get?format=html&results_per_page=1",
+		json: true
+	}, function (error, response, body) {
+    	if (!error && response.statusCode === 200) {
+            var src = body.match(/src="(.*)"/mi);
+    	  	bot.sendMessage(msg.chat.id, ''+ src[1]);
+		}
+	});
+
 	}
     //Отмазки
     if(msg.text.indexOf('!отмазка') != -1) {
@@ -36,5 +51,6 @@ bot.on('message', function (msg) {
 		var name = msg.text.replace('!Психотерапия ', '');
 	  	bot.sendMessage(msg.chat.id, Psihoterapia(name));	
 	}  
+	}
 
 });
