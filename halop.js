f0b87e9f141071d80f030b96eb748aafc30d4384
Tor <(Brisstor@yahoo.com)>
2016-04-21 02:12:40 +0300

    Added weather command

--- halop.js
+++ halop.js
@@ -1,11 +1,12 @@
 var TelegramBot = require('node-telegram-bot-api');
-var token = require('./token');
+// var token = require('./token');
 var request = require("request");
 var Generator = require('./excusesgenerator');
 var Psihoterapia = require('./psihoterapia');
 var Like = require('./likes');
 
-var bot = new TelegramBot(token, {polling: true});
+
+var bot = new TelegramBot("175288885:AAGfHoCdOXp6xx0cX8vpUA3sn-fb01G6Ajg", {polling: true});
 
 //Читаем сообщения)
 bot.on('message', function (msg) {
@@ -35,6 +36,21 @@ bot.on('message', function (msg) {
 	if(msg.text.indexOf('!Психотерапия') != -1) {
 		var name = msg.text.replace('!Психотерапия ', '');
 	  	bot.sendMessage(msg.chat.id, Psihoterapia(name));	
-	}  
-
+	}
+	//Погода
+	if(msg.text.indexOf('!Погода') != -1){
+		var length =  msg.text.split(' ').length,
+			city;
+		length === 1 ? city = 'moscow' : city = msg.text.replace('!Погода ', '');
+		request({
+			url:"http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=12f31155bc369a43ba9fd8589a6fbb41",
+			json: true
+		}, function (error,response,body) {
+			if (!error && response.statusCode === 200) {
+				bot.sendMessage(msg.chat.id,'Текущая температура ' + body.main.temp + ' °С,' +
+					'\nМинимальная температура сегодня ' + body.main.temp_min + ' °С' +
+					'\nМаксимальная температура сегодня ' + body.main.temp_max + ' °С');
+			}
+		})
+	}
 });
